0e616a15019843a668fb1d1633886860220f52ff
Lorans <lorans.chirko@comodo.od.ua>
2015-10-08 12:45:22 +0300

    optimized store

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
-
+var raw = [];
 var store = [];
-PDFJS.getDocument('compressed.tracemonkey-pldi-09.pdf').then(function (pdf) {
+PDFJS.getDocument('pdf.pdf').then(function (pdf) {
     pdf.getPage(1).then(function (page) {
         var scale = 1.5;
         var viewport = page.getViewport(scale);
@@ -22,27 +21,45 @@ PDFJS.getDocument('compressed.tracemonkey-pldi-09.pdf').then(function (pdf) {
         }
 
         Object.getOwnPropertyNames(context.__proto__).forEach(function (val, idx, array) {
-            if (typeof context[val] === 'function')
+            if (typeof context[val] === 'function') {
                 override(context, val, function (original) {
                     return function () {
                         original.apply(this, arguments);
-                        store.push({method: val, data: arguments});
+                        raw.push({method: val, data: arguments}); // raw calls without optimization
+
+                        if (store[store.length - 1].method == val) {
+                            store[store.length - 1].data.push(arguments);
+                        } else {
+                            store.push({method: val, data: [arguments]});
+                        }
                     }
                 });
-            store.push({property: val, data: context[val]});
+            } else {
+                raw.push({property: val, data: context[val]});
+                store.push({property: val, data: context[val]});
+            }
         });
 
         page.render(renderContext).then(function () {
             var myCanvas = document.getElementById('myCanvas');
             var myContext = myCanvas.getContext('2d');
+
             console.log(store);
+            var rawLength=JSON.stringify(raw).length;
+            var storeLength=JSON.stringify(store).length;
+            console.log("Raw length: " + rawLength);
+            console.log("Store length: " + storeLength);
+            console.log("Less on : " + ((1-storeLength/rawLength)*100).toFixed(2)+"%");
             store.forEach(function (item, i, arr) {
                 if (i == 0) {
                     myCanvas.height = item.data.height;
                     myCanvas.width = item.data.width;
                 } else {
                     if (item.hasOwnProperty("method") && item.method != "fillRect") {
-                        myContext[item.method].apply(myContext, item.data);
+                        item.data.forEach(function (params, j, dataArray) {
+                            myContext[item.method].apply(myContext, params);
+                        });
+
                     } else if (item.hasOwnProperty("property")) {
                         myContext[item.property] = item.data;
                     }
@@ -61,6 +78,7 @@ PDFJS.getDocument('compressed.tracemonkey-pldi-09.pdf').then(function (pdf) {
                     mcanvas.getContext("2d").drawImage(img, 0, 0);
                 };
             }
+
             var canvas = document.getElementById('the-canvas');
             var context = canvas.getContext('2d');
             var data = canvas.toDataURL();
